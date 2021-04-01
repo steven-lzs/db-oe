50dc5d96be69f21170288079023efd4462120db2
Stevenlzs <steven.lau@challenger.sg>
2021-04-01 16:44:18 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Main from './containers/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi from me.
         </p>
-      </header>
+      </header> */}
+      <Main />
     </div>
   );
 }
