cd4ead958527f52385a434302c264cec96eea680
Stevenlzs <steven.lau@challenger.sg>
2021-04-10 19:46:17 +0800

    .

--- website/craco.config.js
+++ website/craco.config.js
@@ -1,24 +1,8 @@
-const path = require('path')
-
 // craco.config.js
 module.exports = {
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
-  },
-  publicPath: '/',
-  chainWebpack: (config) => {
-    config.module.rule('eslint').use('eslint-loader').options({
-      fix: true,
-    })
-  },
-  configureWebpack: {
-    resolve: {
-      extensions: ['.js', '.vue', '.json', '.scss'],
-      alias: {
-        styles: path.resolve(__dirname, 'src/assets/scss'),
-      },
-    },
-  },
+  }
 }
